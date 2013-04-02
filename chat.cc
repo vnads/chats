3f36d54a5eaa3fc89613774088f5e74dd176fe45
Vamsi Nadimpalli <vamsi.nadimpalli@gmail.com>
2013-04-01 22:05:19 -0500

    getting one-way chat

--- chat.cc
+++ chat.cc
@@ -9,66 +9,84 @@
 #include <string.h>
 
 void *CheckForMessages(void* ptr) {
-	Message* msg;
-	msg = (Message*)ptr;
-	int count = 0;
+	const char *readFile;
+	readFile = (char*)ptr;
+	const mode_t mode = 0666;
+	const int openFlags = (O_RDWR);
+	int fdread = open(readFile, openFlags, mode);
+	
+	if(fdread == (-1)) {
+		printf("reader open returned (-1)\n");
+		return NULL;
+	}
+
+	Message* read = Message::GetFromMemoryMappedFile(fdread);
 	while(1) {
-        char *message = msg->DequeueMessage();
-        printf("Other Dude: %s", message);
-        //fflush(stdout);
+        char *message = read->DequeueMessage();
+        printf("Other Dude: %s\n", message);
+       	fflush(stdout);
     }
+
+	Message::ReleaseFile(read, fdread);
+		
+    close(fdread);
 }
 
 int main(int argc, char* argv[]) {
 	const mode_t mode = 0666;
     const int openFlags = (O_CREAT | O_TRUNC | O_RDWR);
 	
-	int fdread = 0;
-	int fdwrite = 0;
-	Message* read;
-	Message* write;
+	char *readFromFile;	
+	char *writeToFile;
 
 	if(!strcmp(argv[1], "1")) {
-		const char *readFromFile = "shared2.dat";
-		const char *writeToFile = "shared1.dat";
-		fdwrite = open(writeToFile, openFlags, mode);
-		fdread = open(readFromFile, openFlags, mode);
-		if(fdread == (-1) || fdwrite == (-1)) {
-			printf("open returned (-1) copy\n");
+		readFromFile = "shared2.dat";
+		writeToFile = "shared1.dat";
+		
+		//create the read file
+		int fdread = open(readFromFile, openFlags, mode);
+	
+		if(fdread == (-1)) {
+			printf("reader open returned (-1)\n");
 			return (-1);
 		}
-		write = Message::CopyToMemoryMappedFile(fdwrite);
-		read = Message::CopyToMemoryMappedFile(fdread);
+
+		Message* read = Message::CopyToMemoryMappedFile(fdread);
+		//now release it
+		Message::ReleaseFile(read, fdread);
+		
+    	close(fdread);
+	
 	}
 	else {
-		const char *readFromFile = "shared1.dat";
-		const char *writeToFile = "shared2.dat";
-		fdwrite = open(writeToFile, openFlags, mode);
-		fdread = open(readFromFile, openFlags, mode);
-		if(fdread == (-1) || fdwrite == (-1)) {
-			printf("open returned (-1) get\n");
-			return (-1);
-		}
-		read = Message::GetFromMemoryMappedFile(fdread);
-		write = Message::GetFromMemoryMappedFile(fdwrite);        
-
+		readFromFile = "shared1.dat";
+		writeToFile = "shared2.dat";
 	}	                    		
+
+	int fdwrite = open(writeToFile, openFlags, mode);
+	if(fdwrite == (-1)) {
+		printf("writer open returned (-1)\n");
+		return (-1);
+	}
+
+	Message* write = Message::CopyToMemoryMappedFile(fdwrite);
 	
 	pthread_t t1;
 	int thread; 
 
-    thread = pthread_create(&t1, NULL, &CheckForMessages, (void*)read);
-	pthread_join(t1, NULL);
+    thread = pthread_create(&t1, NULL, &CheckForMessages, (void*)readFromFile);
+	//pthread_join(t1, NULL);
 
 	while(1) {
-		std::string s;
-		getline(std::cin, s);
-		
+		std::string str;
+		getline(std::cin, str);
+	    char *cstr = new char[str.length() + 1];
+		strcpy(cstr, str.c_str());
+		write->EnqueueMessage(cstr);	
 	}
 
-    Message::ReleaseFile(read, fdread);
+    
 	Message::ReleaseFile(write, fdwrite);
-    close(fdread);
 	close(fdwrite);
 }
 
